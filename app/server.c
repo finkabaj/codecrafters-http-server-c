af7550560fefab59a09e97be0d729b8711dbd54b
finkabaj <yuriy9505@gmail.com>
2024-06-21 20:39:12 +0500

    codecrafters submit [skip ci]

--- app/server.c
+++ app/server.c
@@ -16,6 +16,7 @@ char *response_text_plain = "HTTP/1.1 200 OK\r\nContent-Type: "
 char *response_octec_stream =
     "HTTP/1.1 200 OK\r\nContent-Type: "
     "application/octet-stream\r\nContent-Length: %d\r\n\r\n%s";
+char *response_created = "HTTP/1.1 201 Created\r\n\r\n";
 
 struct HandleConnectionArgs {
   int client_fd;
@@ -23,10 +24,15 @@ struct HandleConnectionArgs {
   char *dir;
 };
 
+char *find_substring_between(const char *str, const char *start_str,
+                             const char *end_str);
 void *handle_connection(void *arg);
 char *handle_echo(char *endpoint, int client_fd, int server_fd);
-char *handle_user_agent(char orig_buffer[], int client_fd, int server_fd);
-char *handle_files(char *filename, char *dir, int client_fd, int server_fd);
+char *handle_user_agent(char orig_buffer[BUF_SIZE], int client_fd,
+                        int server_fd);
+char *handle_get_files(char *filename, char *dir, int client_fd, int server_fd);
+char *handle_post_files(char *filename, char *dir, char orig_buffer[BUF_SIZE],
+                        int client_fd, int server_fd);
 
 int main(int argc, char **argv) {
   // Disable output buffering
@@ -43,7 +49,6 @@ int main(int argc, char **argv) {
       }
 
       dir = argv[i];
-      printf("%s\n", dir);
     } else {
       printf("invalid flag");
       exit(EXIT_FAILURE);
@@ -133,32 +138,41 @@ void *handle_connection(void *arg) {
   }
 
   int buf_len = strlen(request_buffer);
-  char orig_buffer[buf_len];
+  char orig_buffer[BUF_SIZE];
   memcpy(orig_buffer, request_buffer, buf_len);
 
   char *path = strtok(request_buffer, " ");
+  char *method = path;
   path = strtok(NULL, " ");
 
   char *endpoint = strtok(path, "/");
   char *reply = NULL;
 
-  if (endpoint == NULL) {
+  if (endpoint == NULL && strcmp(method, "GET") == 0) {
     reply = response_ok;
-  } else if (strcmp(endpoint, "user-agent") == 0) {
+  } else if (endpoint != NULL && strcmp(endpoint, "user-agent") == 0 &&
+             strcmp(method, "GET") == 0) {
     reply = handle_user_agent(orig_buffer, client_fd, server_fd);
-  } else if (strcmp(endpoint, "echo") == 0 &&
+  } else if (endpoint != NULL && strcmp(endpoint, "echo") == 0 &&
+             strcmp(method, "GET") == 0 &&
              (endpoint = strtok(NULL, "/")) != NULL) {
     reply = handle_echo(endpoint, client_fd, server_fd);
-  } else if (strcmp(endpoint, "files") == 0 &&
+  } else if (endpoint != NULL && strcmp(endpoint, "files") == 0 &&
+             strcmp(method, "GET") == 0 &&
              (endpoint = strtok(NULL, "/")) != NULL) {
-    reply = handle_files(endpoint, dir, client_fd, server_fd);
+    reply = handle_get_files(endpoint, dir, client_fd, server_fd);
+  } else if (endpoint != NULL && strcmp(endpoint, "files") == 0 &&
+             strcmp(method, "POST") == 0 &&
+             (endpoint = strtok(NULL, "/")) != NULL) {
+    reply = handle_post_files(endpoint, dir, orig_buffer, client_fd, server_fd);
   } else {
     reply = response_not_found;
   }
 
   int bytes_sent = send(client_fd, reply, strlen(reply), 0);
 
-  if (reply != response_ok && reply != response_not_found) {
+  if (reply != response_ok && reply != response_not_found &&
+      reply != response_created) {
     free(reply);
   }
 
@@ -210,13 +224,15 @@ char *handle_user_agent(char orig_buffer[], int client_fd, int server_fd) {
   return reply;
 }
 
-char *handle_files(char *filename, char *dir, int client_fd, int server_fd) {
+char *handle_get_files(char *filename, char *dir, int client_fd,
+                       int server_fd) {
   char *reply = NULL;
   size_t len;
 
+  // TODO: validate path
+
   char path[strlen(filename) + strlen(dir) + 2];
   sprintf(path, "%s/%s", dir, filename);
-  printf("path: %s\n", path);
 
   FILE *fp;
   if ((fp = fopen(path, "r")) == NULL) {
@@ -231,6 +247,7 @@ char *handle_files(char *filename, char *dir, int client_fd, int server_fd) {
   fseek(fp, 0, SEEK_SET);
   if ((f_buff = (char *)malloc(len)) == NULL) {
     printf("Memory allocation failed: %s\n", strerror(errno));
+    fclose(fp);
     return response_not_found;
   }
   fread(f_buff, 1, len, fp);
@@ -241,10 +258,95 @@ char *handle_files(char *filename, char *dir, int client_fd, int server_fd) {
 
   if ((reply = (char *)malloc(reply_size)) == NULL) {
     printf("Memory allocation failed: %s\n", strerror(errno));
+    free(f_buff);
     return response_not_found;
   }
   sprintf(reply, response_octec_stream, len, f_buff);
   free(f_buff);
 
   return reply;
+}
+
+char *handle_post_files(char *filename, char *dir, char orig_buffer[BUF_SIZE],
+                        int client_fd, int server_fd) {
+  char *content_length_str = NULL;
+  if ((content_length_str = find_substring_between(
+           orig_buffer, "Content-Length: ", "\r\n")) == NULL) {
+    printf("handle_post_files: Content-Length header not found\n");
+    return response_not_found;
+  }
+
+  int content_length = atoi(content_length_str);
+  free(content_length_str);
+
+  if (content_length <= 0 || content_length > BUF_SIZE) {
+    printf("handle_post_files: invalid content length\n");
+    return response_not_found;
+  }
+
+  char *content_start = strstr(orig_buffer, "\r\n\r\n");
+  if (!content_start) {
+    printf("handle_post_files: couldn't find start of content\n");
+    return response_not_found;
+  }
+  content_start += 4;
+
+  char *path = NULL;
+  if ((path = (char *)malloc((strlen(filename) + strlen(dir) + 2) *
+                             sizeof(char))) == NULL) {
+    printf("handle_post_files: allocation failed\n");
+    return response_not_found;
+  }
+  sprintf(path, "%s/%s", dir, filename);
+
+  FILE *fp = fopen(path, "wx");
+
+  if (!fp) {
+    if (errno == EEXIST) {
+      printf("handle_post_files: file %s already exists\n", filename);
+    } else {
+      printf("handle_post_files: error creating %s file: %s\n", filename,
+             strerror(errno));
+    }
+    free(path);
+    return response_not_found;
+  }
+
+  size_t written = fwrite(content_start, 1, content_length, fp);
+  fclose(fp);
+  free(path);
+
+  if (written != content_length) {
+    printf("handle_post_files: error while writing to file %s\n", filename);
+    return response_not_found;
+  }
+
+  return response_created;
+}
+
+char *find_substring_between(const char *str, const char *start_str,
+                             const char *end_str) {
+  char *start = strstr(str, start_str);
+  if (start == NULL) {
+    return NULL;
+  }
+
+  start += strlen(start_str);
+
+  char *end = strstr(start, end_str);
+  if (end == NULL) {
+    return NULL;
+  }
+
+  size_t len = end - start;
+
+  char *result = (char *)malloc(len + 1);
+  if (result == NULL) {
+    return NULL;
+  }
+
+  strncpy(result, start, len);
+  result[len] = '\0';
+
+  return result;
 }
